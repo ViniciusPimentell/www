a784b6169a0e03730c4324c2a552c4a7ed8fce85
Marcos Vinicius <m.vini0236@gmail.com>
2024-01-15 18:44:04 -0300

    Projeto Alura 1

---
+++ app.js
@@ -0,0 +1,67 @@
+let listasDeNumerosSorteasdos = [];
+let numeroLimite = 10;
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+    responsiveVoice.speak(texto,'Brazilian Portuguese Female',
+    {rate:1.0});
+}
+
+exibirTextoNaTela('h1', 'Jogo do número secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'ACERTOU');
+        let palavraTentativa = tentativas > 1 ? 'tentativas' :
+            'tentativa';
+        let mensagemTentativas = `Você descobriu o número secreto com
+    ${tentativas} ${palavraTentativa}!`;
+        exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute
+            ('disabled');
+    } else {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela('p', 'O número é menor');
+        } else {
+            exibirTextoNaTela('p', 'O número é maior');
+        }
+        tentativas++;
+        limparCampo()
+    }
+}
+
+function gerarNumeroAleatorio() {
+    let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
+    let quantidadeDeElementosNaLista = listasDeNumerosSorteasdos.length;
+
+    if (quantidadeDeElementosNaLista == numeroLimite){
+        listasDeNumerosSorteasdos = [];
+    } 
+
+    if (listasDeNumerosSorteasdos.includes(numeroEscolhido)) {
+        return gerarNumeroAleatorio();
+    } else {
+        listasDeNumerosSorteasdos.push(numeroEscolhido)
+        console.log(listasDeNumerosSorteasdos)
+        return numeroEscolhido
+    }
+}
+
+function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    exibirTextoNaTela('h1', 'Jogo do número secreto')
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10')
+}
